023aa34ff9a38690f9eb16e3e7285dc460cc80fc
Musa <falcondesignagency@gmail.com>
2023-09-26 18:30:41 +0200

    commit

--- src/main/java/com/example/musicify2/CorsConfig.java
+++ src/main/java/com/example/musicify2/CorsConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.musicify2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,40 +33,3 @@ public class CorsConfig {
         return new CorsFilter(corsConfigurationSource());
     }
 }
-=======
-package com.example.musicify2;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://allowed-origin-1.com");
-        configuration.addAllowedOrigin("http://127.0.0.1:5500");
-        // Add more allowed origins as needed
-        configuration.addAllowedMethod("GET");
-        configuration.addAllowedMethod("POST");
-        configuration.addAllowedMethod("PUT");
-        configuration.addAllowedMethod("DELETE");
-        // Add more allowed methods as needed
-        configuration.addAllowedHeader("*"); // Allow all headers
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
-}
->>>>>>> 9bf18060dcea291b64fd9848ff6ec78d47c64970
