de9db5adc88373f50b1317b6210aa25a9756179c
Musa <falcondesignagency@gmail.com>
2023-09-26 21:12:04 +0200

    allow cors

--- src/main/java/com/example/musicify2/AppController.java
+++ src/main/java/com/example/musicify2/AppController.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@CrossOrigin
 public class AppController {
 
     @GetMapping("/upload")
--- src/main/java/com/example/musicify2/RestMainController.java
+++ src/main/java/com/example/musicify2/RestMainController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin
 public class RestMainController {
 
     MainService service;
