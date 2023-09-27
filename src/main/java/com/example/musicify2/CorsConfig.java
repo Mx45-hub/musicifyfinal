91981ce3d35952223bb04d3200e018d429da79f2
Musa <falcondesignagency@gmail.com>
2023-09-27 14:59:39 +0200

    cors change

--- src/main/java/com/example/musicify2/CorsConfig.java
+++ src/main/java/com/example/musicify2/CorsConfig.java
@@ -1,27 +1,25 @@
 package com.example.musicify2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@EnableWebSecurity
 public class CorsConfig {  
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://127.0.0.1:5500");
-        configuration.addAllowedOrigin("http://localhost:5500");
-        configuration.addAllowedOrigin("https://musicifyfront.onrender.com");
-        configuration.addAllowedOrigin("https://musicapp-dquz.onrender.com");
+        configuration.addAllowedOrigin("*"); // Allow all origins
         configuration.addAllowedMethod("GET");
         configuration.addAllowedMethod("POST");
         configuration.addAllowedMethod("PUT");
         configuration.addAllowedMethod("DELETE");
-        // Add more allowed methods as needed
-        configuration.addAllowedHeader("header1"); // Allow all headers
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
